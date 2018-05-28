27c44717e3f56b7efcd9b545e1014caaf94b6a91
Victor Korir <victor@vics-mac.gateway.sonic.net>
2018-05-28 10:52:16 -0700

    resolved Content Security Policy Error

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 // Data layer control (Redux)
 // import "materialize-css/dist/css/materialize.min.css";
 // import "./styles/custom.css";
-import "./styles/foundation.css";
-import "./styles/index.css";
+// import "./styles/foundation.css";
+// import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
